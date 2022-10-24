c6761daaef8582610ebcaa62489a9271be1c64f4
fedexxF <fefedeq@gmail.com>
2022-10-24 16:59:19 -0300

    Deply BackEnd

--- src/main/java/Porfolio/BackEnd/controller/BodyController.java
+++ src/main/java/Porfolio/BackEnd/controller/BodyController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins =("http://porfolio-10e59.web.app/") )
 @RequestMapping("/body")
 public class BodyController {
     private final BodyService bodyService;
